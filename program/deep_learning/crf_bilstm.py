9a596689b8d87ba41e4b07adae2dff080ec2d411
windsuzu <windsuzu@gmail.com>
2020-12-22 21:48:26 +0800

    [Add] tensorflow2 bi-lstm crf

---
+++ program/deep_learning/crf_bilstm.py
@@ -0,0 +1,360 @@
+#%%
+import tensorflow as tf
+import tensorflow_addons as tf_ad
+import os
+
+
+def build_vocab(corpus_file_list, vocab_file, tag_file):
+    """
+    利用 train, test 建立 vocab file 和 tag file
+
+    Args:
+        corpus_file_list:
+            [text_file_path]
+
+        vocab_file:
+            output_vocab_file_path
+
+        tag_file:
+            output_tag_file_path
+
+    Output:
+        vocab_file.txt
+        tag.txt
+    """
+    words = set()
+    tags = set()
+    for file in corpus_file_list:
+        # words = words.union(set([line.strip().split()[0]  for line in open(file, "r", encoding='utf-8').readlines()]))
+        # tags = tags.union(set([line.strip().split()[-1] for line in open(file, "r", encoding='utf-8').readlines()]))
+        for line in open(file, "r", encoding="utf-8").readlines():
+            line = line.strip()
+            if line == "end":
+                continue
+            try:
+                w, t = line.split()
+                words.add(w)
+                tags.add(t)
+            except Exception as e:
+                print(line.split())
+                # raise e
+
+    if not os.path.exists(vocab_file):
+        with open(vocab_file, "w", encoding="utf-8") as f:
+            for index, word in enumerate(["<UKN>"] + list(words)):
+                f.write(word + "\n")
+
+    tag_sort = {
+        "O": 0,
+        "B": 1,
+        "I": 2,
+        "E": 3,
+    }
+
+    tags = sorted(
+        list(tags),
+        key=lambda x: (
+            len(x.split("-")),
+            x.split("-")[-1],
+            tag_sort.get(x.split("-")[0], 100),
+        ),
+    )
+    if not os.path.exists(tag_file):
+        with open(tag_file, "w", encoding="utf-8") as f:
+            for index, tag in enumerate(["<UKN>"] + tags):
+                f.write(tag + "\n")
+
+
+def read_vocab(vocab_file):
+    """
+    讀取 vocab_file，回傳 vocab <-> id 雙向的字典
+
+    Args:
+        vocab_file:
+            input_vocab_file_path
+
+    Returns:
+        vocab2id:
+            {'<UKN>': 0,
+             '扎': 1,
+             '遺': 2,
+             ...}
+
+        id2vocab:
+            {0: '<UKN>',
+             1: '扎',
+             2: '遺',
+             ...}
+    """
+    vocab2id = {}
+    id2vocab = {}
+    for index, line in enumerate(
+        [line.strip() for line in open(vocab_file, "r", encoding="UTF8").readlines()]
+    ):
+        vocab2id[line] = index
+        id2vocab[index] = line
+    return vocab2id, id2vocab
+
+
+def tokenize(filename, vocab2id, tag2id):
+    """
+    給定 train.data 輸出 tokenize 過的文字列表
+
+    Args:
+        filename:
+            train.data
+            中 O
+            文 O
+
+        vocab2id:
+            voc -> id dictionary
+
+        tag2id:
+            tag -> id dictionary
+
+    Returns:
+        contents:
+            array([[ 158, 2076, 1865, ...,    0,    0,    0],
+                   [ 158, 2076, 1865, ...,    0,    0,    0],
+                   [1019, 1023, 1865, ...,    0,    0,    0],
+                   ...,
+                   [ 158, 2076, 1865, ...,    0,    0,    0]])
+
+        labels:
+            array([[1, 1, 1, ..., 0, 0, 0],
+                   [1, 1, 1, ..., 0, 0, 0],
+                   [1, 1, 1, ..., 0, 0, 0],
+                   ...,
+                   [1, 1, 1, ..., 0, 0, 0]])
+    """
+    contents = []
+    labels = []
+    content = []
+    label = []
+    with open(filename, "r", encoding="utf-8") as fr:
+        for line in [elem for elem in fr.readlines()][:500000]:
+            try:
+                if line != "\n":
+                    w, t = line.split()
+                    content.append(vocab2id.get(w, 0))
+                    label.append(tag2id.get(t, 0))
+                else:
+                    if content and label:
+                        contents.append(content)
+                        labels.append(label)
+                    content = []
+                    label = []
+            except Exception as e:
+                content = []
+                label = []
+    contents = tf.keras.preprocessing.sequence.pad_sequences(contents, padding="post")
+    labels = tf.keras.preprocessing.sequence.pad_sequences(labels, padding="post")
+    return contents, labels
+
+
+tag_check = {
+    "I": ["B", "I"],
+    "E": ["B", "I"],
+}
+
+
+def check_label(front_label, follow_label):
+    if not follow_label:
+        raise Exception("follow label should not both None")
+
+    if not front_label:
+        return True
+
+    if follow_label.startswith("B-"):
+        return False
+
+    if (
+        (follow_label.startswith("I-") or follow_label.startswith("E-"))
+        and front_label.endswith(follow_label.split("-")[1])
+        and front_label.split("-")[0] in tag_check[follow_label.split("-")[0]]
+    ):
+        return True
+    return False
+
+
+def format_result(chars, tags):
+    """
+    將 TEXT 和 TAG 抓出來，回傳 entity 列表。
+
+    Args:
+        chars: ['国','家','发','展','计','划','委','员','会','副','主','任','王','春','正']
+
+        tags: ['B-ORG', 'I-ORG', 'I-ORG', 'I-ORG', 'I-ORG', 'I-ORG', 'I-ORG', 'I-ORG', 'E-ORG', 'O', 'O', 'O', 'B-PER', 'I-PER', 'E-PER']
+
+    Returns:
+        [{'begin': 1, 'end': 9, 'words': '国家发展计划委员会', 'type': 'ORG'},
+         {'begin': 13, 'end': 15, 'words': '王春正', 'type': 'PER'}]
+    """
+
+    entities = []
+    entity = []
+    for index, (char, tag) in enumerate(zip(chars, tags)):
+        entity_continue = check_label(tags[index - 1] if index > 0 else None, tag)
+        if not entity_continue and entity:
+            entities.append(entity)
+            entity = []
+        entity.append([index, char, tag, entity_continue])
+    if entity:
+        entities.append(entity)
+
+    entities_result = []
+    for entity in entities:
+        if entity[0][2].startswith("B-"):
+            entities_result.append(
+                {
+                    "begin": entity[0][0] + 1,
+                    "end": entity[-1][0] + 1,
+                    "words": "".join([char for _, char, _, _ in entity]),
+                    "type": entity[0][2].split("-")[1],
+                }
+            )
+
+    return entities_result
+
+
+# %%
+class NerModel(tf.keras.Model):
+    def __init__(self, hidden_num, vocab_size, label_size, embedding_size):
+        super(NerModel, self).__init__()
+        self.num_hidden = hidden_num
+        self.vocab_size = vocab_size
+        self.label_size = label_size
+
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_size)
+        self.biLSTM = tf.keras.layers.Bidirectional(
+            tf.keras.layers.LSTM(hidden_num, return_sequences=True)
+        )
+        self.dense = tf.keras.layers.Dense(label_size)
+
+        self.transition_params = tf.Variable(
+            tf.random.uniform(shape=(label_size, label_size))
+        )
+        self.dropout = tf.keras.layers.Dropout(0.5)
+
+    @tf.function
+    def call(self, text, labels=None, training=None):
+        text_lens = tf.math.reduce_sum(
+            tf.cast(tf.math.not_equal(text, 0), dtype=tf.int32), axis=-1
+        )
+        # -1 change 0
+        inputs = self.embedding(text)
+        inputs = self.dropout(inputs, training)
+        logits = self.dense(self.biLSTM(inputs))
+
+        if labels is not None:
+            label_sequences = tf.convert_to_tensor(labels, dtype=tf.int32)
+            log_likelihood, self.transition_params = tf_ad.text.crf_log_likelihood(
+                logits,
+                label_sequences,
+                text_lens,
+                transition_params=self.transition_params,
+            )
+            return logits, text_lens, log_likelihood
+        else:
+            return logits, text_lens
+
+
+# %%
+vocab_file_path = "../../dataset/crf_bilstm/vocab_file.txt"
+tag_file_path = "../../dataset/crf_bilstm/tag.txt"
+
+train_path = "../../dataset/crf_data/train.data"
+
+if not (os.path.exists(vocab_file_path) and os.path.exists(tag_file_path)):
+    build_vocab([train_path], vocab_file_path, tag_file_path)
+# %%
+voc2id, id2voc = read_vocab(vocab_file_path)
+tag2id, id2tag = read_vocab(tag_file_path)
+
+text_sequences, label_sequences = tokenize(train_path, voc2id, tag2id)
+
+BATCH_SIZE = 2
+train_dataset = tf.data.Dataset.from_tensor_slices((text_sequences, label_sequences))
+train_dataset = train_dataset.shuffle(len(text_sequences)).batch(
+    BATCH_SIZE, drop_remainder=True
+)
+
+HIIDEN_NUMS = 512
+EMBEDDING_SIZE = 300
+LEARNING_RATE = 1e-3
+EPOCHS = 10
+checkpoint_file_path = "../checkpoints/crf_bilstm/"
+
+model = NerModel(
+    hidden_num=HIIDEN_NUMS,
+    vocab_size=len(voc2id),
+    label_size=len(tag2id),
+    embedding_size=EMBEDDING_SIZE,
+)
+
+optimizer = tf.keras.optimizers.Adam(LEARNING_RATE)
+
+ckpt = tf.train.Checkpoint(optimizer=optimizer, model=model)
+ckpt.restore(tf.train.latest_checkpoint(checkpoint_file_path))
+ckpt_manager = tf.train.CheckpointManager(
+    ckpt, checkpoint_file_path, checkpoint_name="model.ckpt", max_to_keep=3
+)
+
+
+# %%
+@tf.function
+def train_one_step(text_batch, labels_batch):
+    with tf.GradientTape() as tape:
+        logits, text_lens, log_likelihood = model(
+            text_batch, labels_batch, training=True
+        )
+        loss = -tf.reduce_mean(log_likelihood)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    return loss, logits, text_lens
+
+
+def get_acc_one_step(logits, text_lens, labels_batch):
+    paths = []
+    accuracy = 0
+    for logit, text_len, labels in zip(logits, text_lens, labels_batch):
+        viterbi_path, _ = tf_ad.text.viterbi_decode(
+            logit[:text_len], model.transition_params
+        )
+        paths.append(viterbi_path)
+        correct_prediction = tf.equal(
+            tf.convert_to_tensor(
+                tf.keras.preprocessing.sequence.pad_sequences(
+                    [viterbi_path], padding="post"
+                ),
+                dtype=tf.int32,
+            ),
+            tf.convert_to_tensor(
+                tf.keras.preprocessing.sequence.pad_sequences(
+                    [labels[:text_len]], padding="post"
+                ),
+                dtype=tf.int32,
+            ),
+        )
+        accuracy = accuracy + tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        # print(tf.reduce_mean(tf.cast(correct_prediction, tf.float32)))
+    accuracy = accuracy / len(paths)
+    return accuracy
+
+
+#%%
+best_acc = 0
+step = 0
+
+for epoch in range(EPOCHS):
+    for _, (text_batch, labels_batch) in enumerate(train_dataset):
+        step = step + 1
+        loss, logits, text_lens = train_one_step(text_batch, labels_batch)
+        if step % 20 == 0:
+            accuracy = get_acc_one_step(logits, text_lens, labels_batch)
+
+            if accuracy > best_acc:
+                best_acc = accuracy
+                ckpt_manager.save()
+
+# %%
--- program/machine_learning/crf_baseline.py
+++ program/machine_learning/crf_baseline.py
@@ -1,6 +1,7 @@
 # %% import block
 import os
 import numpy as np
+import sys
 
 import sklearn_crfsuite
 from sklearn_crfsuite import scorers
@@ -9,6 +10,7 @@ from sklearn.model_selection import train_test_split
 from sklearn_crfsuite.metrics import flat_classification_report
 
 from pathlib import Path
+
 sys.path.append(str(Path().resolve().parents[1]))
 
 from program.utils.load_data import loadInputFile, loadTestFile, generateCRFFormatData
@@ -16,11 +18,18 @@ from program.utils.write_output_file import generateOutputFile
 
 validate = False
 test_data_delete_blank = True
-train_file_path = "data/train_0_half.txt"
-test_file_path = "data/test_half.txt"
-train_data_path = "data/trainFormat.data"
-test_data_path = "data/testFormat.data"
-pretrained_word2vec_path = 'data/cna.cbow.cwe_p.tar_g.512d.0.txt'
+# train_file_path = "data/train_0_half.txt"
+# test_file_path = "data/test_half.txt"
+train_file_path = "../../dataset/train.txt"
+test_file_path = "../../dataset/test.txt"
+
+# train_data_path = "data/trainFormat.data"
+# test_data_path = "data/testFormat.data"
+train_data_path = "../../dataset/crf_data/train.data"
+test_data_path = "../../dataset/crf_data/test.data"
+
+
+pretrained_word2vec_path = "data/cna.cbow.cwe_p.tar_g.512d.0.txt"
 output_path = "output/output.tsv"
 
 # %%
@@ -44,15 +53,15 @@ model = sklearn_crfsuite.CRF(
 # get a dict of tokens (key) and their pretrained word vectors (value)
 # pretrained word2vec CBOW word vector:
 # https://fgc.stpi.narl.org.tw/activity/videoDetail/4b1141305ddf5522015de5479f4701b1
-word_vecs= {}
-with open(pretrained_word2vec_path, 'r', encoding='utf-8') as word2vec:
+word_vecs = {}
+with open(pretrained_word2vec_path, "r", encoding="utf-8") as word2vec:
     for line in word2vec:
         tokens = line.strip().split()
 
         # There are two integers in the first line: vocabulary_size, word_vector_dim
         if len(tokens) == 2:
             continue
-    
+
         word = tokens[0]
         vec = np.array([float(dim) for dim in tokens[1:]])
         word_vecs[word] = vec
@@ -63,38 +72,41 @@ print("word_vector_dim: ", vec.shape)
 
 # %% Fit required data sturture of crf model
 def CRFFormatToList(data_path):
-    
-    with open(data_path, 'r', encoding='utf-8') as format_data:
-        data = format_data.readlines()#.encode('utf-8').decode('utf-8-sig')
-    
-    state = "test" if len(data[0].strip('\n').split(' ')) == 1 else "train"
+
+    with open(data_path, "r", encoding="utf-8") as format_data:
+        data = format_data.readlines()  # .encode('utf-8').decode('utf-8-sig')
+
+    state = "test" if len(data[0].strip("\n").split(" ")) == 1 else "train"
     total_data_list, separate_data_list = list(), list()
     for row in data:
         data_tuple = tuple()
-        if row == '\n':
+        if row == "\n":
             total_data_list.append(separate_data_list)
             separate_data_list = []
         else:
             if state == "test":
-                token = row.strip('\n').split(' ')
+                token = row.strip("\n").split(" ")
                 separate_data_list.append(token)
             else:
-                token, label = row.strip('\n').split(' ')
+                token, label = row.strip("\n").split(" ")
                 token_label_pair = (token, label)
                 separate_data_list.append(token_label_pair)
-    
+
     return total_data_list
+
 
 def GetUntokenVector(word_vector_dim):
     np.random.seed(42)
     unk_vector = np.random.rand(word_vector_dim)
     return unk_vector
 
+
 def ListToDict(vector):
     vector_dict = dict()
     for idx_vec in range(len(vector)):
-        vector_dict['dim_' + str(idx_vec+1)] = vector[idx_vec]
+        vector_dict["dim_" + str(idx_vec + 1)] = vector[idx_vec]
     return vector_dict
+
 
 def Word2Vector(separate_data_list, embedded_dict, unk_vector):
     separate_embedded_list = list()
@@ -107,8 +119,9 @@ def Word2Vector(separate_data_list, embedded_dict, unk_vector):
             vector = unk_vector
         vector = ListToDict(vector)
         separate_embedded_list.append(vector)
-        
+
     return separate_embedded_list
+
 
 def GetInputData(data_list, embedded_dict):
     input_list = list()
@@ -118,10 +131,13 @@ def GetInputData(data_list, embedded_dict):
     unk_vector = GetUntokenVector(word_vector_dim)
 
     for idx_list in range(len(data_list)):
-        separate_separate_list = Word2Vector(data_list[idx_list], embedded_dict, unk_vector)
+        separate_separate_list = Word2Vector(
+            data_list[idx_list], embedded_dict, unk_vector
+        )
         input_list.append(separate_separate_list)
 
     return input_list
+
 
 def GetLabelData(data_list):
     total_label_list = list()
@@ -132,6 +148,7 @@ def GetLabelData(data_list):
         total_label_list.append(separate_label_list)
     return total_label_list
 
+
 # %%
 train_data_list = CRFFormatToList(train_data_path)
 test_data_list = CRFFormatToList(test_data_path)
@@ -139,7 +156,9 @@ test_data_list = CRFFormatToList(test_data_path)
 # %%
 if validate:
     print("Processing validate data")
-    train_data_list, val_data_list = train_test_split(train_data_list, test_size=0.25, random_state=42)
+    train_data_list, val_data_list = train_test_split(
+        train_data_list, test_size=0.25, random_state=42
+    )
     x_val = GetInputData(val_data_list, word_vecs)
     y_val = GetLabelData(val_data_list)
 
@@ -157,16 +176,27 @@ y_pred = model.predict(x_test)
 # %%
 def ModelEvaluation(model, ground_truth, predict):
     labels = list(model.classes_)
-    labels.remove('O')
-    f1_score = metrics.flat_f1_score(ground_truth, predict, average='weighted', labels=labels)
-    sorted_labels = sorted(labels, key=lambda name: (name[1:], name[0])) # group B and I results
-    
-    print(flat_classification_report(ground_truth, predict, labels=sorted_labels, digits=3))
+    labels.remove("O")
+    f1_score = metrics.flat_f1_score(
+        ground_truth, predict, average="weighted", labels=labels
+    )
+    sorted_labels = sorted(
+        labels, key=lambda name: (name[1:], name[0])
+    )  # group B and I results
+
+    print(
+        flat_classification_report(
+            ground_truth, predict, labels=sorted_labels, digits=3
+        )
+    )
     print("F1 score :", f1_score)
+
 
 if validate:
     y_pred_val = model.predict(x_val)
     ModelEvaluation(model, y_val, y_pred_val)
 
 # %% Output in upload format
-GenerateOutputFile(y_pred, test_data_list, testingset, output_path, test_data_delete_blank)
+GenerateOutputFile(
+    y_pred, test_data_list, testingset, output_path, test_data_delete_blank
+)
